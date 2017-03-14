689fe986fe3b4a60be4ef7a83621bcfb233cee42
Andres Salguero <andressalguero55@gmail.com>
2017-03-13 23:34:53 -0400

    estableciendo el primer cambio

--- app/src/main/java/com/videotutoriales/avisosmarco/AvisosActivity.java
+++ app/src/main/java/com/videotutoriales/avisosmarco/AvisosActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class AvisosActivity extends AppCompatActivity {
 
+//Primer commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
